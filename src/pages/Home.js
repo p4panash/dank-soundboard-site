2c13f1c528395929190bd4a390a49d52d8fe8afd
Catalin Muntean <muntean.catalin.avram@gmail.com>
2021-05-20 17:48:24 +0300

    Add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import "./Home.css";
 
 class Home extends React.Component {
   render() {
     return(
       <div className="Home">
-        <h1>Hello World</h1>
+        <div className="Title">
+          <h1 className="Title-name">DankBoard Bot</h1>
+          <img className="Title-hero" src="./bonk_doge-large.png"/>
+          <p className="Title-description">Such sounds, very board</p>
+
+          <a href="https://discord.com/oauth2/authorize?client_id=752158811438972968&scope=bot">
+            <button>Add To Server</button>
+          </a>
+        </div>
       </div>
     );
   }
