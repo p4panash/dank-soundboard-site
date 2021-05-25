713e225f73caa3667620fb03a0377b1578030ee4
Catalin Muntean <muntean.catalin.avram@gmail.com>
2021-05-25 15:44:57 +0300

    Finish home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import "./Home.css";
 
-class Home extends React.Component {
-  render() {
-    return(
-      <div className="Home">
-        <div className="Title">
-          <h1 className="Title-name">DankBoard Bot</h1>
-          <img className="Title-hero" src="./bonk_doge-large.png"/>
-          <p className="Title-description">Such sounds, very board</p>
+function Home() {
+  const [isButtonFocused, setIsButtonFocused] = useState(false);
+  const [dankText, setDankText] = useState("WoW")
+  const texts = ["WoW", "Y u still here ?", "kek", '&#127814;']
 
-          <a href="https://discord.com/oauth2/authorize?client_id=752158811438972968&scope=bot">
-            <button>Add To Server</button>
-          </a>
-        </div>
-      </div>
-    );
+  const tryMe = (
+    <div className="Idiom">
+      <div className="arrow-up"></div>
+      <p style={{marginTop: "0px"}}>Press there!</p>
+    </div>
+  )
+  const dankIdiom = (
+    <div className="Idiom">
+      <p>{dankText}</p>
+    </div>
+  )
+
+  const onMouseEvent = (isFocused) => {
+    if (isFocused) {
+      setDankText(texts[Math.floor((Math.random() * texts.length) - 1)])
+    }
+    setIsButtonFocused(isFocused)
   }
+
+  return(
+    <div className="Home">
+      <div className="Title">
+        <h1 className="Title-name">DankBoard Bot</h1>
+        <p className="Title-description">Such sounds, very board</p>
+        <img className="Title-hero" src="./bonk_doge-large.png" alt="Doge meme"/>
+
+        <a href="https://discord.com/oauth2/authorize?client_id=752158811438972968&scope=bot">
+          <button id="inviteButton"
+            onMouseEnter={() => onMouseEvent(true)}
+            onMouseLeave={() => onMouseEvent(false)}>
+            Add To Server
+          </button>
+        </a>
+
+        {isButtonFocused ? dankIdiom : tryMe}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
