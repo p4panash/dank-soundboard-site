f17586e9f6e425f04c1ff79c4c389af1854bea67
Catalin Muntean <muntean.catalin.avram@gmail.com>
2021-08-25 18:15:15 +0300

    Add new pics and choose randome one for each component

--- src/components/DankImages.js
+++ src/components/DankImages.js
@@ -7,6 +7,7 @@ function DankImages() {
   let width = useCurrentWidth();
   const [elements, setElements] = useState(null);
   const [start, setStart] = useState(0);
+  const memes = ["chad", "doge", "dorito", "face", "frog", "hitmark", "pepe", "pikachu", "spoderman", "spongebob", "weed"]
 
   useEffect(() => {
     setElements(null);
@@ -23,7 +24,8 @@ function DankImages() {
       for (var i = 1 * start; i < start + amount; i++) {
         let time = Math.floor(Math.random() * 4) + 2;
         let iterations = 12 / time;
-        elem_arr.push(<BounceImage source="/image.png" key={i} index={i} duration={time + "s"} iteration={iterations} 
+        let ri = Math.floor((Math.random() * memes.length))
+        elem_arr.push(<BounceImage source={`/${memes[ri]}.png`} key={i} index={i} duration={time + "s"} iteration={iterations} 
           marginLeftValue={margin_left} animationName={time === 4 ? "bounce" : "bounce, spin"} firstChild={i === 1 * start}/>)
       }
       setElements(elem_arr);
